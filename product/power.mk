c561d1e46df9e4c408f8c7131f5b6b17f1845f5d
Michael Bestas <mkbestas@lineageos.org>
2018-09-17 22:43:53 +0300

    rolex: Switch to 1.1 power HAL
    Change-Id: I4690a5b913141427a828d6a3a4af407d35a10775

--- product/power.mk
+++ product/power.mk
@@ -1,6 +1,6 @@
 # Power IMPL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.1-service-qti
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power_profile_riva.xml:$(TARGET_COPY_OUT_VENDOR)/etc/power_profile_riva.xml
